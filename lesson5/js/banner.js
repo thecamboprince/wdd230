d728f7ad5bb5236f1984ecdafbe0501e04d68a52
thecamboprince <dug21002@byui.edu>
2021-10-27 03:25:27 -0600

    final commit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,7 +1,7 @@
 let banner = document.querySelector('#banner');
 let dayOfWeek = new Date().getDay();
 // if Friday displays this
-if (dayOfWeek == 3) {
+if (dayOfWeek == 5) {
     banner.style.display = "block";
 }
 
